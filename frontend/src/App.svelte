c5d47e45c4127806a24f777b3ca849a6a8976883
Wes West <jmwest@gmail.com>
2023-08-11 14:42:01 -0700

    It's aliiiiive!  It more or less works.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -7,13 +7,49 @@
   let outputText = "";
   let dataIs = "plural"; // default value
 
+  const sample1 = `In today's digital age, data is crucial. Data drives most decisions, from business strategies to personal choices. However, there's often confusion about how to treat the word "data" in written English.
+
+Traditionally, "data" is the plural form of "datum." This means that when referring to multiple pieces of information, one would say, "The data are clear." On the other hand, if referring to a single piece of information, the correct usage would be, "This datum is clear."
+
+However, modern usage has evolved. Many people now treat "data" as a singular noun, especially in the context of digital information. For example, "The data is being processed" or "This data shows a significant trend."
+
+Google's style guide provides clarity on many such issues. It's a comprehensive resource that offers guidance on grammar, punctuation, and word choice. Adhering to a style guide ensures consistency and clarity in written communication.
+
+This app respects individual preferences. Users can choose whether they want to treat "data" as a singular or plural noun. This customization ensures that documents align with the writer's intent.
+
+For instance, consider the sentence, "The data is compelling." If a user prefers the traditional approach, the tool flags this as an error. Conversely, if the preference is for modern usage, the sentence "The data show a rising trend" would be highlighted.
+
+In conclusion, language is dynamic, and its usage evolves over time. While it's essential to respect traditional rules, it's equally important to acknowledge contemporary practices. The tool aims to bridge this gap, offering flexibility while maintaining adherence to the Google style guide.
+
+This text triggers the singular or plural rule for "data" based on the user's preference. If the user chooses the traditional approach (data as plural), the sentences "The data is being processed" and "The data is compelling" is flagged. If the user opts for the modern usage (data as singular), the sentence "The data show a rising trend" is highlighted.`; 
+  const sample2 = `In a world where numbers never lie,
+The data are a beacon, shining bright.
+Once data were obscured, now they fly,
+Revealing truths, bringing them to light.
+
+Data have secrets, whispered in the night,
+Yet data themselves can sometimes deceive.
+In the dance of figures, data do unite,
+Telling tales that few can believe.
+
+But data is more than just cold hard facts,
+For once data was a story untold.
+Now, data has a voice, and it acts,
+With power, data itself does hold.
+
+In the end, data does what it must,
+To earn our ever-growing trust.`; 
+
   function evaluateText() {
     console.log("evaluateText function called with input:", inputText);
-    // You can now pass the dataIs variable to the CheckWithVale function if needed
     CheckWithVale(inputText, dataIs).then(result => {
         console.log("Received result from backend:", result);
         outputText = result;
     });
+  }
+
+  function loadSample(sample) {
+    inputText = sample;
   }
 </script>
 
@@ -25,10 +61,21 @@
       align-items: center;
   }
 
-  textarea {
+  .input-section {
+      display: flex;
+      align-items: start;
       width: 90%;
+  }
+
+  textarea {
+      flex: 1;
       height: 200px;
-      margin-bottom: 20px;
+      margin-right: 20px;
+  }
+
+  .sample-buttons {
+      display: flex;
+      flex-direction: column;
   }
 
   button {
@@ -40,12 +87,31 @@
       border: 1px solid #ccc;
       padding: 10px;
       white-space: pre-line;
+      text-align: left; 
+  }
+
+  .rules {
+      text-align: left;
+      font-size: 0.8rem;
   }
 </style>
 
 <div class="container">
   <div>
-      <p>Static text that describes how to use the application.</p>
+      <h2>Assignment:</h2>
+      <p>
+        Take on the role of the company's data engineers in building a product prototype. It should be a desktop application that can be distributed as a single executable load module for Windows 10/11, Apple Mac, or Linux computers. It should be capable of reading a plain text file of at least 200 words and checking for spelling and grammatical errors. But, more importantly, it should be customizable, with the ability to adapt to the writing style of each individual user/writer.
+      </p>
+      <p>
+        To demonstrate the ability to conform to the styles of individual writers, the application should ask users whether they want to treat "data" as a singular or plural noun. (Note that proper English has "data" as a plural noun. The singular of "data" is "datum."). When presented with a document in which the word "data" is used incorrectly (as defined by user preference), the sentence containing the word should be flagged as erroneous.
+      </p>
+      <h2>How to Use</h2>
+      <p>
+        Enter text and click evaluate. The only rules turned on are the data singular / plural, as defined by the radio boxes below. Please see the bottom for the specific rules that are captured.
+      </p>
+      <p>
+        If you want some sample text to get you started, please click the samples on the right.
+      </p>
       <label>
         <input type="radio" bind:group={dataIs} value="singular">
         'Data' is Singular
@@ -59,7 +125,23 @@
         Produce All Warnings
       </label>
   </div>
-  <textarea bind:value={inputText} placeholder="Enter your test here..."></textarea>
+  <div class="input-section">
+      <textarea bind:value={inputText} placeholder="Enter your text here..."></textarea>
+      <div class="sample-buttons">
+          <button on:click={() => loadSample(sample1)}>Sample 1</button>
+          <button on:click={() => loadSample(sample2)}>Sample 2</button>
+      </div>
+  </div>
   <button on:click={evaluateText}>Evaluate</button>
   <div class="output">{outputText}</div>
+  <div class="rules">
+      <h3>This program adheres to the following rules:</h3>
+      <ul>
+          <li>Data is / are</li>
+          <li>Data was / were</li>
+          <li>Data has / have</li>
+          <li>Data itself / themselves</li>
+          <li>Data does / do</li>
+      </ul>
+  </div>
 </div>
